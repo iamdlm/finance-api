74247ba8ebf1a44ea3f235181bc3cfef46edce19
iamdlm <diogoluismesquita@outlook.com>
2022-02-01 22:28:56 +0000

    Add filter for setting base entity properties

--- src/Fin/FinApi/Data/FinDbContext.cs
+++ src/Fin/FinApi/Data/FinDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FinApi
@@ -13,6 +14,36 @@ namespace FinApi
 
         public FinDbContext(DbContextOptions<FinDbContext> options) : base(options) { }
 
-        public DbSet<User> Users { get; set; }        
+        public DbSet<User> Users { get; set; }
+
+        public override int SaveChanges()
+        {
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            AddTimestamps();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void AddTimestamps()
+        {
+            var entities = ChangeTracker.Entries()
+                .Where(x => (x.Entity is BaseEntity) && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            DateTime now = DateTime.Now;
+
+            foreach (var entity in entities)
+            {
+                if (entity.State == EntityState.Added)
+                {
+                    ((BaseEntity)entity.Entity).Created = now;                    
+                }
+
+                ((BaseEntity)entity.Entity).Modified = now;                
+            }
+        }
     }
 }
