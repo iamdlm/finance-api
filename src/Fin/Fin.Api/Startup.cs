9623157d305f81a4e8d622359fcb91a966603d93
iamdlm <diogoluismesquita@outlook.com>
2022-02-05 16:30:08 +0000

    Refactor DbContext startup

--- src/Fin/Fin.Api/Startup.cs
+++ src/Fin/Fin.Api/Startup.cs
@@ -27,8 +27,7 @@ namespace FinApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<FinDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDbContext(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddDbContext(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(setup =>
